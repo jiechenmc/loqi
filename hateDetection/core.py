5199c22c7547b6789c2ba9affe49a00a4ab4b7c2
Jie Chen <jiechenmc@gmail.com>
2022-10-03 00:21:43 -0400

    allow model to be downloaded during installation

--- hateDetection/core.py
+++ hateDetection/core.py
@@ -3,3 +3,7 @@ from detoxify import Detoxify
 
 def toxicity_of(message):
     return Detoxify('original').predict(message)["severe_toxicity"]
+
+
+if __name__ == "__main__":
+    Detoxify('original').predict("")
