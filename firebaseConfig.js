2c9ae2b45191d599e4ad2c0b9ae5d84e1cf8529f
Jie Chen <jiechenmc@gmail.com>
2022-10-13 00:15:44 -0400

    removed console.log

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -7,6 +7,7 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
 const firebaseConfig = {
   apiKey: process.env.FB_apiKey,
   authDomain: process.env.FB_authDomain,
