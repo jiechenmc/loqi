124ac2f9582f30602585183c86d5454f397de69b
Jie Chen <jiechenmc@gmail.com>
2022-10-16 02:02:47 -0400

    beginning of signin logic

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,5 +1,6 @@
-import useEmailAndPassword from "./hooks/useSignUpEmailAndPassword";
 import { Dispatch, SetStateAction, useRef, useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "./App";
 
 interface LoginPageInterface {
   setLoginStatus: Dispatch<SetStateAction<boolean>>;
@@ -11,9 +12,35 @@ const Login = ({ setLoginStatus }: LoginPageInterface) => {
   const emailRef = useRef<HTMLInputElement>(null);
   const passwordRef = useRef<HTMLInputElement>(null);
 
-  setLoginStatus(useEmailAndPassword(email, password));
-
   const handleSignUp = () => {
+    if (emailRef.current && passwordRef.current) {
+      const em = emailRef.current.value;
+      const pw = passwordRef.current.value;
+
+      setEmail(em);
+      setPassword(pw);
+
+      emailRef.current.value = "";
+      passwordRef.current.value = "";
+    }
+
+    createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        setLoginStatus(true);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+  };
+
+  const handleSignIn = () => {
     if (emailRef.current && passwordRef.current) {
       const em = emailRef.current.value;
       const pw = passwordRef.current.value;
@@ -40,6 +67,9 @@ const Login = ({ setLoginStatus }: LoginPageInterface) => {
         <input ref={passwordRef} className="border-2" type="text"></input>
         <button onClick={handleSignUp} className="border-2">
           Sign Up
+        </button>
+        <button onClick={handleSignIn} className="border-2">
+          Sign In
         </button>
       </form>
     </div>
