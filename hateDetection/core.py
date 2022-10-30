df23bfc15c7b3c62a06d5b8bcf8cb6b0a50a0cf6
Jie Chen <jiechenmc@gmail.com>
2022-10-30 13:51:26 -0400

    removed hate detection server

--- hateDetection/core.py
+++
@@ -1,9 +0,0 @@
-from detoxify import Detoxify
-
-
-def toxicity_of(message):
-    return Detoxify('original').predict(message)["severe_toxicity"]
-
-
-if __name__ == "__main__":
-    Detoxify('original').predict("")
